22d5397972712e7afc70f9b1a2919383697edf86
CallsignNavy <callsignnavy.business@gmail.com>
2025-06-28 22:02:00 -0400

    Added Character Counts to output

--- main.py
+++ main.py
@@ -7,12 +7,14 @@ def get_book_text(filepath):
 #pull from stats.py
 from stats import get_word_count
 
+from stats import get_char_count
+
 #print the number of words from the book in a string response
 def main():
 	book_text = get_book_text("books/frankenstein.txt")
 	total_words = get_word_count(book_text)
 	print(f"{total_words} words found in the document")
-
+	print(get_char_count(book_text))
 main()
 
 
--- stats.py
+++ stats.py
@@ -3,3 +3,16 @@ def get_word_count(book_text):
         words = book_text.split()
         num_words = len(words)
         return num_words
+
+#function for character count
+def get_char_count(book_text):
+	indv_char_dict = {}
+	words = book_text.split()
+	for word in words:
+		lower_cased = word.lower()
+		for letter in lower_cased:
+			if letter not in indv_char_dict:
+				indv_char_dict[letter] = 1
+			else:
+				indv_char_dict[letter] += 1
+	return indv_char_dict
