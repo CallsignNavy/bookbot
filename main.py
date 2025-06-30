dc92051718cf4fdd6e8937807465fea201d57260
CallsignNavy <callsignnavy.business@gmail.com>
2025-06-30 00:02:28 -0400

    created a way to take an input for use on searching for books within the directories and output the total words and alphabetical characters

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#needed imports
+import sys
+
 #establish a path to the books location
 def get_book_text(filepath):
 	with open(filepath) as f:
@@ -13,12 +16,16 @@ from stats import (
 
 #print the number of words from the book in a string response
 def main():
-	book_text = get_book_text("books/frankenstein.txt")
+	if len(sys.argv) != 2:
+		print("Usage: python3 main.py <path_to_book>")
+		sys.exit(1)
+	input = sys.argv[1]
+	book_text = get_book_text(f"{input}")
 	total_words = get_word_count(book_text)
 	char_counts = get_char_count(book_text)
 	chars_sorted_list = char_sort(char_counts)
 	print("============ BOOKBOT ============")
-	print("Analyzing book found at books/frankenstein.txt...")
+	print(f"Analyzing book found at {input}...")
 	print("----------- Word Count ----------")
 	print(f"Found {total_words} total words")
 	print("--------- Character Count -------")
